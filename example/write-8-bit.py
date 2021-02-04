e1b52e2bed54894d181488b3bd5d80ee0ce7f071
Harsh Thakur <45368701+HarryKT@users.noreply.github.com>
2021-02-04 11:28:28 +0530

    Update write-8-bit.py

--- example/write-8-bit.py
+++ example/write-8-bit.py
@@ -14,7 +14,7 @@ from RPi import GPIO                        # This is the library which we will 
 
 # Initializing the LCD Display
 lcd = CharLCD(numbering_mode=GPIO.BOARD, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
-lcd.write_string(u'Hello world!')
+lcd.write_string("Hello world!")
 
 # Always Clean Up the GPIO after using the code
 GPIO.cleanup()
