88381d762544b5435cffbbc3764e75aba44e1670
Harsh Thakur <45368701+HarryKT@users.noreply.github.com>
2021-02-04 11:53:52 +0530

    Create time-4-bit.py

---
+++ example/time-4-bit.py
@@ -0,0 +1,20 @@
+## PRINT THE DATE AND TIME
+
+'''
+This program will display the date and time on the LCD:
+'''
+
+import time                                 # This is the library which we will be using for the time function
+from RPLCD import CharLCD                   # This is the library which we will be using for LCD Display 
+from RPi import GPIO                        # This is the library which we will be using for using the GPIO pins of Raspberry PI
+
+# Initializing the LCD Display
+lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+
+while True:
+    lcd.write_string("Time: %s" %time.strftime("%H:%M:%S"))
+    
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("Date: %s" %time.strftime("%m/%d/%Y"))
+    
+# Press Ctrl+C to exit the program.
